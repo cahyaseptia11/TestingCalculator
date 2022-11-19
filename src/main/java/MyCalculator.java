d06be3741459ec5ff619f98a820dbae46d444387
cahya septia <cahyaseptia011@gmail.com>
2022-11-18 23:42:11 -0800

    TestCalculator

---
+++ src/main/java/MyCalculator.java
@@ -0,0 +1,20 @@
+public class MyCalculator {
+    public static int add (int a, int b){
+        return a+b;
+    }
+
+    public static int substrac (int a, int b){
+        return  a-b;
+    }
+    public static int multiply (int a, int b){
+        return a*b;
+    }
+
+    public static int devision (int a, int b){
+        return a/b;
+    }
+
+    public static int modulus (int a, int b){
+        return  a%b;
+    }
+}
